c62c4870300bd6857ac3618abda4f5d66b6bfdf8
mihail.virlan <mihail.virlan@esolutions.ro>
2020-04-27 22:02:39 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('.banner button').addEventListener(
+    'click', () => {
+        document.querySelector('.banner').getElementsByClassName.display = 'none';
+        document.querySelector('.slideshow-wrapper').style.cssText = 'opacity: 1; visibility: visible';
+        document.querySelector('.slideshow').style.animation = 'slideshow 24s infinite';
+})
